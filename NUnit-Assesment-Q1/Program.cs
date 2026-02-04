f39d3d46c8c3c43250780d54870f4ecc69ed6f87
AMAN AGARWAL <120473537+onik2008@users.noreply.github.com>
2026-02-04 14:18:29 +0530

    Implement banking operations with unit tests

--- NUnit-Assesment-Q1/Program.cs
+++ NUnit-Assesment-Q1/Program.cs
@@ -1,1 +1,89 @@
+using System;
+using NUnit.Framework;
 
+
+public class Program
+{
+    public decimal Balance { get; private set; }
+
+    
+    public Program(decimal initialBalance)
+    {
+        Balance = initialBalance;
+    }
+
+    
+    public void Deposit(decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new Exception("Deposit amount cannot be negative");
+        }
+
+        Balance += amount;
+    }
+
+    
+    public void Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+        {
+            throw new Exception("Insufficient funds.");
+        }
+
+        Balance -= amount;
+    }
+
+    
+    public static void Main(string[] args)
+    {
+        
+        
+    }
+}
+
+// NUnit Test Class
+[TestFixture]
+public class UnitTest
+{
+    [Test]
+    public void Test_Deposit_ValidAmount()
+    {
+        Program account = new Program(1000);
+        account.Deposit(500);
+
+        Assert.AreEqual(1500, account.Balance);
+    }
+
+    [Test]
+    public void Test_Deposit_NegativeAmount()
+    {
+        Program account = new Program(1000);
+
+        Assert.AreEqual(
+            "Deposit amount cannot be negative",
+            Assert.Throws<Exception>(() => account.Deposit(-200)).Message
+        );
+    }
+
+
+    [Test]
+    public void Test_Withdraw_ValidAmount()
+    {
+        Program account = new Program(1000);
+        account.Withdraw(400);
+
+        Assert.AreEqual(600, account.Balance);
+    }
+
+    [Test]
+    public void Test_Withdraw_InsufficientFunds()
+    {
+        Program account = new Program(500);
+
+        Assert.AreEqual(
+            "Insufficient funds.",
+            Assert.Throws<Exception>(() => account.Withdraw(800)).Message
+        );
+    }
+}
